53dc1432b861b2b939c552c526a318093e5af724
KiyonoOsawa <kiyo.sntr.x6@icloud.com>
2024-08-27 10:41:53 +0900

    だん

---
+++ app.rb
@@ -0,0 +1,36 @@
+require 'bundler/setup'
+Bundler.require
+require 'sinatra/reloader' if development?
+require './models'
+
+get '/' do
+    @medicine = Medicine.all
+    erb :index
+end
+
+get "/item/create" do
+    erb :new
+end
+
+post "/item/create" do
+    Medicine.create(name: params[:name], amount: params[:amount])
+    redirect '/'
+end
+
+get "/item/:id" do
+    @id = params[:id]
+    @medicine = Medicine.find(params[:id])
+    erb :item
+end
+
+post "/item/:id" do
+    medicine = Medicine.find(params[:id])
+    medicine.update(name: params[:name], amount: params[:amount])
+    redirect "/"
+end
+
+get "/item/:id/delete" do
+    medicine = Medicine.find(params[:id])
+    medicine.destroy
+    redirect "/"
+end
